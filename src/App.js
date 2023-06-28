9a02ea9b394814ed93e84272c30afa47c703e68e
DongBui <buivandong@kng.vn>
2023-06-28 18:02:12 +0700

    UPDATE

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
                     {publicRoutes.map((route, index) => {
                         const Layout = route.layout === null ? Fragment : DefaultLayout;
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />;
+                        return <Route 
+                        key={index} 
+                        path={route.path} 
+                        element={<Layout><Page /></Layout>} 
+                        />;
                     })}
                 </Routes>
             </div>
