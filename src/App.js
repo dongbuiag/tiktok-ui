abe54401919de1ba9e25a9b1615f9d077389194f
DongBui <buivandong@kng.vn>
2023-06-28 18:00:26 +0700

    Aaaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>React App</h1>
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Layout><Page /></Layout>} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
