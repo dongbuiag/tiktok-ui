2a688646cac36e14c1e396118076f86e00cba066
DongBui <buivandong@kng.vn>
2023-06-30 18:22:02 +0700

    #65 Header custom ep.3

---
+++ src/components/Button/index.js
@@ -0,0 +1,64 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  text = false,
+  rounded = false,
+  disabled = false,
+  small = false,
+  large = false,
+  children,
+  className,
+  onClick,
+  ...passProps
+}) {
+  let Comp = 'button';
+  const props = {
+    onClick,
+    ...passProps,
+  };
+
+  //Remove event listener when button disabled
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+
+  if (to) {
+    props.to = to;
+    Comp = Link;
+  } else if (href) {
+    props.href = href;
+    Comp = 'a';
+  }
+
+  const classes = cx('wrapper', {
+    [className]: className,
+    // Khi co className thi se lay gia tri cua className lam KEY cho [className]
+    primary,
+    outline,
+    text,
+    rounded,
+    disabled,
+    small,
+    large,
+  });
+
+  return (
+    <Comp className={classes} {...props}>
+      <span>{children}</span>
+    </Comp>
+  );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -7,7 +7,8 @@ import Tippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import AccountItem from '../../AccountItem';
+import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
@@ -51,7 +52,10 @@ function Header() {
             </button>
           </div>
         </Tippy>
-        <div className={cx('actions')}></div>
+        <div className={cx('actions')}>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+        </div>
       </div>
     </header>
   );
